8be6274dea34e3c578011d03605d9147ffa0f09b
ZoDSeR SyLVaN <30656356+ZoDSeR97@users.noreply.github.com>
2024-10-05 14:18:09 -0400

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # photomong-photobooth
 A Full-Stack photobooth project
+
+## :white_check_mark: Requirements
+- python -m django --version
+5.0.6
+
+- node --version
+v22.9.0
+
+## :checkered_flag: How to run ##
+
+## 🤡 Frontend
+
+- React
+
+## 👾 Backend
+
+- Django
+- SQlite
---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+cd backend
+start cmd /k "pip install -r requirements.txt && python manage.py runserver"
+cd ../app
+start cmd /k "npm install && npm start"
+start "C:\Program Files\Google\Chrome\Application\chrome.exe --allow-file-access-from-files -kiosk -fullscreen"
