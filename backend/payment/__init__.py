19e6007c9e343d0f9a7ef2abd92bd108fac3c395
ZoDSeR SyLVaN <30656356+ZoDSeR97@users.noreply.github.com>
2024-10-13 11:30:41 -0400

    [WIP] payment merge

--- backend/payment/__init__.py
+++ backend/payment/__init__.py
@@ -1,18 +1,1 @@
-""" import serial
-import serial.tools.list_ports
-
-# Find Arduino port
-def find_arduino_port():
-    ports = list(serial.tools.list_ports.comports())
-    for p in ports:
-        if 'Arduino' in p.description:
-            return p.device
-    return None
-
-# Initialize serial communication with Arduino
-arduino_port = find_arduino_port()
-if arduino_port:
-    ser = serial.Serial(arduino_port, 9600, timeout=1)
-    logging.info(f"Arduino connected on {arduino_port}")
-else:
-    logging.error("Arduino not found. Please check the connection.") """
+default_app_config = 'payments.apps.PaymentsConfig'
--- backend/payment/apps.py
+++ backend/payment/apps.py
@@ -1,6 +1,31 @@
 from django.apps import AppConfig
-
+import serial
+import serial.tools.list_ports
+import logging
 
 class PaymentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'payment'
+    
+    def ready(self):
+        self.initialize_arduino()
+
+    def initialize_arduino(self):
+        # Arduino Initialization Logic
+        logging.info("Initializing Arduino...")
+
+        def find_arduino_port():
+            ports = list(serial.tools.list_ports.comports())
+            for p in ports:
+                if 'Arduino' in p.description:
+                    return p.device
+            return None
+
+        arduino_port = find_arduino_port()
+        if arduino_port:
+            global ser  # Make the serial connection accessible globally
+            ser = serial.Serial(arduino_port, 9600, timeout=1)
+            logging.info(f"Arduino connected on {arduino_port}")
+        else:
+            logging.error("Arduino not found. Please check the connection.")
+            raise Exception("Arduino not found")
