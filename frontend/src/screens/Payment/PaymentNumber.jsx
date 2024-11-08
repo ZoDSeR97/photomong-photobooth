81f35117252cf4ae90a7a813d6003a1d894f6c29
ZoDSeR SyLVaN <zodser97@gmail.com>
2024-11-08 16:38:41 +0700

    adjust price

--- frontend/src/screens/Payment/PaymentNumber.jsx
+++ frontend/src/screens/Payment/PaymentNumber.jsx
@@ -134,7 +134,7 @@ function PaymentNumber(props) {
   const getDong = () => {
     let amount = 0, add = 20000;
     if(import.meta.env.VITE_BOOTH_TYPE == "REG"){
-      amount = 80000
+      amount = 70000
     } else {
       amount = 100000
     }
