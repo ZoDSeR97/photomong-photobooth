ec3c66faad1af9e2ea2f8058ce19bc104cc9cbbf
ZoDSeR SyLVaN <zodser97@gmail.com>
2024-11-18 17:20:37 +0700

    adjust script

--- run.bat
+++ run.bat
@@ -2,6 +2,11 @@
 set batch1="%~dp0bindingArduino.bat"
 set batch2="%~dp0bindingCam.bat"
 
+start wsl -e bash -c "cd; exec bash"
+
+:: Wait for the batch files to finish, if needed (optional delay or pause)
+timeout /t 5 > nul
+
 :: Execute each batch file with admin privileges
 PowerShell -Command "Start-Process cmd -ArgumentList '/c %batch1%' -Verb RunAs"
 PowerShell -Command "Start-Process cmd -ArgumentList '/c %batch2%' -Verb RunAs"
