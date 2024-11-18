f4945ef62591ce54742d71aa5a07c230b86faa5e
ZoDSeR SyLVaN <zodser97@gmail.com>
2024-11-18 16:11:49 +0700

    minor adjustment

--- Flask/camera.py
+++ Flask/camera.py
@@ -16,13 +16,13 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
 
 # Globals
-frame_queue = queue.Queue(maxsize=60)
+frame_queue = queue.Queue(maxsize=180)
 LIVE_VIEW_ACTIVE = False
 VIDEO_WRITER_ACTIVE = False
 video_writer = None
 video_lock = threading.Lock()
 live_view_thread = None
-PREVIEW_INTERVAL = 0.1  # 100ms between frames
+PREVIEW_INTERVAL = 0.0167  # 17ms between frames
 
 class CameraManager:
     def __init__(self):
@@ -162,6 +162,16 @@ class CameraManager:
             except gp.GPhoto2Error as e:
                 logging.error(f"Error in live view: {e}")
                 continue
+    
+    def generate_frames(self):
+        """Generate frames for live view."""
+        while True:
+            try:
+                frame = self.frame_queue.get()
+                _, buffer = cv2.imencode('.jpg', frame)
+                yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n\r\n')
+            except queue.Empty:
+                continue
 
     def _write_video_frame(self, frame):
         """Write frame to the video file."""
@@ -254,16 +264,6 @@ class CameraManager:
             self.start_live_view()
             return {'status': 'error', 'message': 'Failed to capture image after multiple attempts'}
 
-    def generate_frames(self):
-        """Generate frames for live view."""
-        while True:
-            try:
-                frame = self.frame_queue.get(timeout=5)
-                _, buffer = cv2.imencode('.jpg', frame)
-                yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
-            except queue.Empty:
-                continue
-
 # Flask endpoints
 camera_manager = CameraManager()
 
@@ -273,8 +273,9 @@ def video_feed():
 
 @app.route('/api/start_live_view', methods=['GET'])
 def start_live_view():
-    camera_manager.start_live_view()
-    #camera_manager.start_video_recording()
+    if not LIVE_VIEW_ACTIVE:
+        camera_manager.start_live_view()
+        #camera_manager.start_video_recording()
     return jsonify(status="Live view and video recording started")
 
 @app.route('/api/stop_live_view', methods=['GET'])
@@ -331,6 +332,194 @@ def serve_photo(file_path):
         return send_file(file_path, mimetype="image/png")
     else:
         return jsonify({'status': 'error', 'message': 'File not found'}), 404
+
+# Print image using rundll32
+def print_image_with_rundll32(image_path, frame_type):
+    try:
+        printer_name = 'DS-RX1 (Photostrips)' if frame_type == 'stripx2' else 'DS-RX1'
+        logging.info(f"Printing to {printer_name}")
+        
+        # Print the image using rundll32
+        print_command = f"powershell.exe Start-Process 'rundll32.exe' -ArgumentList 'C:\\Windows\\System32\\shimgvw.dll,ImageView_PrintTo', '\"/pt\"', '{image_path}', '{printer_name}'"
+        print(print_command)
+        logging.debug(f"Executing print command: {print_command}")
+        
+        for i in range(print_amount):
+            subprocess.run(print_command, check=True, shell=True)
+        logging.info(f"Print command sent for file: {image_path}")
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Error printing file: {e}")
+        raise
+
+@app.route('/api/switch-printer/<printer_model>/<frame_type>/', methods=['POST'])
+def switch_printer(printer_model, frame_type):
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    safe_filename = werkzeug.utils.secure_filename(file.filename)
+    temp_dir = os.path.join(f"{os.getcwd()}/print_files")
+    file_path = os.path.join(temp_dir, safe_filename)
+    file_path = file_path.replace('/','\\')
+    print(file_path)
+    file_path = f"\\\wsl$\\Ubuntu{file_path}"
+    file.save(file_path)
+
+    try:
+        print_image_with_rundll32(file_path, frame_type)
+    except Exception as e:
+        logging.error(f"Error processing print job: {e}")
+        return jsonify({'error': str(e)}), 500
+    finally:
+        if os.path.exists(file_path):
+            os.remove(file_path)  # Cleanup after use
+    
+    return jsonify({'status': 'success', 'message': 'Print job started successfully.'})
+
+@app.route("/api/print", methods=['POST'])
+def print_photo():
+    try:
+        print("print_photo")
+        folder_path = os.path.join(f"{os.getcwd()}/print_files")
+        folder_path = os.path.abspath(folder_path)
+        print(folder_path)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        frame = request.form['frame']
+        
+        if request.files['photo'] and (request.files['photo'].mimetype.startswith('image/')):
+            image_content = request.files['photo'].read()
+        else:
+            image_content = base64.b64decode(request.files['photo'])
+
+        if not request.files['photo'] or not frame:
+            return jsonify({'error': 'Invalid input'}), 400
+        
+        print_url = ''
+        print_file_name = ''
+        if frame == 'Stripx2':
+            print_file_name = 'stripx2.png'
+            print_url = os.getenv("API_PRINTER_CUT")
+        elif frame == '2cut-x2':
+            print_file_name = 'cutx2.png'
+            print_url = os.getenv("API_PRINTER_2")
+        elif frame == '3-cutx2':
+            print_file_name = 'cutx3.png'
+            print_url = os.getenv("API_PRINTER_3")
+        elif frame == '4-cutx2':
+            print_file_name = 'cutx4.png'
+            print_url = os.getenv("API_PRINTER_4")
+        elif frame == '5-cutx2':
+            print_file_name = 'cutx5.png'
+            print_url = os.getenv("API_PRINTER_5")
+        elif frame == '6-cutx2':
+            print_file_name = 'cutx6.png'
+            print_url = os.getenv("API_PRINTER_6")
+        
+        file_path = os.path.join(folder_path, print_file_name)
+
+        print(111)
+        print("file_path")
+        print(file_path)
+        print(111)
+
+        with open(file_path, 'wb') as destination:
+            destination.write(image_content)
+
+        # 파일이 제대로 저장되었는지 확인
+        if not os.path.exists(file_path):
+            return jsonify({'status':'error', 'message': 'Failed to save the file'}), 500
+        
+        # 파일 크기 확인
+        file_size = os.path.getsize(file_path)
+        if file_size == 0:
+            return jsonify({'status':'error', 'message': 'Saved file is empty'}), 500
+        
+        # Call POST method to printer                
+        with open(file_path, 'rb') as f:
+            response = requests.post(print_url, files={'file': f})
+
+        print(response.status_code)
+        #print(response.text)
+        if response.status_code == 200:
+            return jsonify({'status':'success', 'message': 'Print job started successfully.'}), 200
+        else:
+            return jsonify({'status':'error', 'message': 'Failed to send print request'}), 500
+    except Exception as e:
+        print(e)
+        return jsonify({'status':'error', 'message': 'Failed to send print request'}), 500
+
+# Start cash payment route
+@app.route('/api/cash/start', methods=['POST'])
+def start_cash_payment():
+    global inserted_money, amount_to_pay #stop_thread counter, money
+    data = request.get_json()
+    amount_to_pay = int(data.get('amount', 0))
+    
+    ser.write(b'RESET\n')
+    response = ser.readline().decode('utf-8').strip()
+    
+    with lock:
+        inserted_money = 0  # Reset the inserted money
+
+    return jsonify({"message": "Cash payment started"}), 200
+
+# Check payment status
+@app.route('/api/cash/status', methods=['GET'])
+def check_payment_status():
+    global inserted_money, amount_to_pay
+    baseV = 10000
+    if (os.getenv('REGION')) == 'MN':
+        baseV = 1000
+    with lock:  # Ensure thread-safe access to the serial port
+        try:
+            ser.write(b'CHECK\n')
+            line = ser.readline().decode('utf-8').strip().split(':').pop()
+            print(line)
+            if line.strip().isdigit():
+                inserted_money = int(line)*baseV
+                logging.info(f"Current inserted money: {inserted_money}, Current amount to pay: {amount_to_pay}")
+                if inserted_money < amount_to_pay:
+                    return jsonify({"status": "in progress", "total_money": inserted_money}), 200
+                else:
+                    return jsonify({"status": "complete", "total_money": inserted_money}), 200
+
+        except serial.SerialException as e:
+            logging.error(f"Serial communication error: {e}")
+            return jsonify({"error": "Communication error with Arduino"}), 500
+        except ValueError:
+            return jsonify({"status": "in progress", "total_money": inserted_money}), 200
+
+    return jsonify({"status": "in progress", "total_money": inserted_money}), 200
+
+# Reset bill acceptor
+@app.route('/api/cash/reset', methods=['POST'])
+def reset_bill_acceptor():
+    ser.write(b'RESET\n')
+    response = ser.readline().decode('utf-8').strip()
+    logging.info("Bill acceptor reset")
+    return jsonify({"message": response}), 200
+
+# Stop cash payment
+@app.route('/api/cash/stop', methods=['POST'])
+def stop_cash_payment():
+    global stop_thread
+    ser.write(b'STOP\n')
+    stop_thread = True
+    response = ser.readline().decode('utf-8').strip()
+    logging.info("Cash payment stopped")
+    return jsonify({"message": response}), 200
+
+# Create a cash payment
+@app.route('/api/cash/create', methods=['GET'])
+def create_cash_payment():
+    device = request.args.get('device')
+    amount = request.args.get('amount')
+    order_code = f"{device}_{amount}"
+    return jsonify({"order_code": order_code}), 200
 
 if __name__ == '__main__':
     try:
