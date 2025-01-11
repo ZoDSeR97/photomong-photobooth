2344ff0ce1ae0df2b0d8737c223803c1599dbb52
ZoDSeR SyLVaN <30656356+ZoDSeR97@users.noreply.github.com>
2025-01-11 09:12:32 -0500

    update run script

--- run.bat
+++ run.bat
@@ -2,23 +2,25 @@
 set batch1="%~dp0bindingArduino.bat"
 set batch2="%~dp0bindingCam.bat"
 
+:: Start WSL
 start wsl -e bash -c "cd; cd Flask/; exec bash"
 
-:: Wait for the batch files to finish, if needed (optional delay or pause)
+:: Wait for the WSL to start
 timeout /t 5 > nul
 
 :: Execute each batch file with admin privileges
 PowerShell -Command "Start-Process cmd -ArgumentList '/c %batch1%' -Verb RunAs"
 PowerShell -Command "Start-Process cmd -ArgumentList '/c %batch2%' -Verb RunAs"
 
-cd frontend
-start cmd /k "bun run dev"
+:: Start frontend server in WSL
+start wsl -e bash -c "cd /home/user/frontend; bun run dev; exec bash"
 
-:: Wait for the batch files to finish, if needed (optional delay or pause)
+:: Wait for the frontend to initialize
 timeout /t 5 > nul
 
+:: Start Flask backend in WSL
 echo Starting app.py in WSL with sudo...
 start wsl -e bash -c "cd /home/user/Flask; sudo python3 app.py; exec bash"
 
-:: Run kiosk
+:: Launch kiosk mode
 start chrome.exe --kiosk --touch-events --overscroll-history-navigation=0 http://localhost:5173
